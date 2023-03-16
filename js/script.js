46e8d4238ae505b9a889ef2aba3d3a7d029fbf14
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-03-16 14:56:13 +0000

    Add: Ticket only after "calcola"

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 // input
 // chiedere il numero di km da percorrere e trasformarla in numero
 // chiedere l'eta del passeggero e trasformarla in numero
+let nameSurnameInput;
 
 const submitBtn = document.getElementById("submit-btn");
 submitBtn.addEventListener("click", function() {
-
+    
     // Prendere il valore dell'input
     const nameSurnameInput = document.getElementById("name-surname");
     const nameSurname = nameSurnameInput.value;
-
+    
     const kmToDoInput = document.getElementById("km-to-do");
     const kmToDo = parseFloat(kmToDoInput.value);
-
+    
     const userAgeInput = document.getElementById("user-age");
     const userAge = parseFloat(userAgeInput.value);
-
+    
     console.log(nameSurname, typeof (nameSurname), kmToDo, typeof(kmToDo), userAge, typeof(userAge));
-
+    
     // control
     if (isNaN(kmToDo)) {
         console.log ("ricarica la pagina e inserisci solamente dei numeri")
@@ -44,9 +45,11 @@ submitBtn.addEventListener("click", function() {
     
     // output
     // prezzo con massimo due cifre decimali
+    const ticket = document.getElementById("ticket");
+    ticket.classList.remove("ms_hidden");
 
     if ((isNaN(kmToDo))||(isNaN(userAge))) {
-        document.getElementById("error").innerHTML = (`ricarica la pagina e inserisci solamente dei numeri`);
+        document.getElementById("error").innerHTML = (`ricarica la pagina e inserisci tutti i dati correttamente`);
     } else if (ticketFinalPrice % 2 === 0) {
         document.getElementById("ticket-final-price").innerHTML = (`${ticketFinalPrice}€`);
     } else {
@@ -66,11 +69,21 @@ submitBtn.addEventListener("click", function() {
     userAgeInput.value = "";
 });
 
+// implementazione bottone Annulla
+const clearBtn = document.getElementById("clean-btn");
+clearBtn.addEventListener("click", function() {
+    // Ripulire e nascondere il biglietto
+    const ticket = document.getElementById("ticket");
+    ticket.classList.add("ms_hidden");
+    // Ripulire gli input
+})
 
-// output
+// OUTPUT
+
 const carriage = Math.floor(Math.random() * 8) + 1;
 document.getElementById("carriage-name").innerHTML = carriage;
 
 const cpCode = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
 document.getElementById("cp-code-name").innerHTML = cpCode;
+
 // prezzo con massimo due cifre decimali
